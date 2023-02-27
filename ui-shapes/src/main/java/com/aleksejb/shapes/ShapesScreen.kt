f236e3aad6aa8393dc5541a8faea7c9aa0a5713b
AleksejB <aleksej.bura.d@gmail.com>
2023-02-27 16:46:01 +0000

    alrithm in process of being fixed

--- ui-shapes/src/main/java/com/aleksejb/shapes/ShapesScreen.kt
+++ ui-shapes/src/main/java/com/aleksejb/shapes/ShapesScreen.kt
@@ -1,7 +1,7 @@
 package com.aleksejb.shapes
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.DropdownMenu
@@ -9,16 +9,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.aleksejb.domain_core.model.OutlineType
 import com.aleksejb.ui_core.R
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun ShapesScreen() {
@@ -80,43 +82,30 @@ private fun ShapesScreenContent(
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.normal_100)))
         }
 
+        val size = LocalConfiguration.current.screenWidthDp
+
         Box() {
-            Canvas(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(350.dp),
-            ) {
-                val trianglePath = Path().let {
-                    it.moveTo(0f, this.size.height)
-                    it.lineTo(this.size.width, this.size.height)
-                    it.lineTo(this.size.width / 2, 0f)
-                    it.close()
-                    it
-                }
-
-                drawPath(trianglePath, state.fillColor)
-
-                drawPath(
-                    path = trianglePath,
-                    color = Color.Blue,
-                    style = if (state.outlineType == OutlineType.SOILD) {
-                        Stroke(
-                            width = 15f,
-                            miter = 0f,
-                            cap = StrokeCap.Butt,
-                            join = StrokeJoin.Miter
-                        )
-                    } else {
-                        Stroke(
-                            width = 15f,
-                            miter = 5f,
-                            cap = StrokeCap.Butt,
-                            join = StrokeJoin.Miter,
-                            pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(20f, 10f), phase = 0f)
-                        )
-                    }
-                )
-            }
+            RegularPolygon(
+                numberOfSides = state.numberOfSide,
+                borderStyle = if (state.outlineType == OutlineType.SOILD) {
+                    Stroke(
+                        width = 15f,
+                        miter = 0f,
+                        cap = StrokeCap.Butt,
+                        join = StrokeJoin.Miter
+                    )
+                } else {
+                    Stroke(
+                        width = 15f,
+                        miter = 5f,
+                        cap = StrokeCap.Butt,
+                        join = StrokeJoin.Miter,
+                        pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(20f, 10f), phase = 0f)
+                    )
+                },
+                fillColor = state.fillColor,
+                size = size.dp
+            )
 
             Text(
                 modifier = Modifier.align(Alignment.Center),
@@ -124,6 +113,108 @@ private fun ShapesScreenContent(
                 color = Color.White
             )
         }
+    }
+}
+
+@Composable
+fun RegularPolygon(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    numberOfSides: Int,
+    borderStyle: DrawStyle,
+    fillColor: Color
+) {
+    Canvas(
+        modifier = modifier.size(size),
+    ) {
+        val centerPoint = Pair<Float, Float>(this.size.width / 2, this.size.height / 2)
+        val startPoint = Pair(this.size.width / 2, 0f)
+        val radius = this.size.width / 2
+        val sweepAngle = (360f / numberOfSides)
+
+        val path = Path().let {
+            it.moveTo(centerPoint.first, 0f)
+
+            val polygonEdgePoints = mutableListOf<Pair<Float, Float>>()
+            polygonEdgePoints.add(startPoint)
+
+            var currentSweepAngle = 0f
+            for (i in 2..numberOfSides) {
+                Log.d("TAAAG", "current iteration: $i")
+                //r remains the same
+                //need to add angle
+                currentSweepAngle += sweepAngle
+
+                when {
+                    currentSweepAngle in 0f..90f -> {
+                        Log.d("TAAAG", "currentSweepAngle: $currentSweepAngle")
+                        val angle = 90 - currentSweepAngle
+                        Log.d("TAAAG", "angle: $angle")
+                        val angleInRadians = angle / (2 * 3.14159265359)
+                        Log.d("TAAAG", "angleInRadians: $angleInRadians")
+                        val x = centerPoint.first + (radius * cos(angleInRadians)).toFloat()
+                        Log.d("TAAAG", "x: $x")
+                        val y = centerPoint.second - (radius * sin(angleInRadians)).toFloat()
+                        Log.d("TAAAG", "y: $y")
+                        polygonEdgePoints.add(Pair(x, y))
+                    }
+                    currentSweepAngle > 90f && currentSweepAngle <= 180f -> {
+                        Log.d("TAAAG", "currentSweepAngle: $currentSweepAngle")
+                        val angle = 90 - (currentSweepAngle - 90)
+                        Log.d("TAAAG", "angle: $angle")
+                        val angleInRadians = angle / (2 * 3.14159265359)
+                        Log.d("TAAAG", "angleInRadians: $angleInRadians")
+                        val x = centerPoint.first + (radius * sin(angleInRadians)).toFloat()
+                        Log.d("TAAAG", "x: $x")
+                        val y = centerPoint.second + (radius * cos(angleInRadians)).toFloat()
+                        Log.d("TAAAG", "y: $y")
+                        polygonEdgePoints.add((Pair(x, y)))
+                    }
+                    currentSweepAngle > 180f && currentSweepAngle <= 270f -> {
+                        Log.d("TAAAG", "currentSweepAngle: $currentSweepAngle")
+                        val angle = currentSweepAngle - 180
+                        Log.d("TAAAG", "angle: $angle")
+                        val angleInRadians = angle / (2 * 3.14159265359)
+                        Log.d("TAAAG", "angleInRadians: $angleInRadians")
+                        val x = centerPoint.first - (radius * sin(angleInRadians)).toFloat()
+                        Log.d("TAAAG", "x: $x")
+                        val y = centerPoint.second + (radius * cos(angleInRadians)).toFloat()
+                        Log.d("TAAAG", "y: $y")
+                        polygonEdgePoints.add(Pair(x, y))
+                    }
+                    currentSweepAngle > 270f && currentSweepAngle <= 360 -> {
+                        if (currentSweepAngle == 360f) {
+
+                        } else {
+                            Log.d("TAAAG", "currentSweepAngle: $currentSweepAngle")
+                            val angle = currentSweepAngle - 270
+                            Log.d("TAAAG", "angle: $angle")
+                            val angleInRadians = angle / (2 * 3.14159265359)
+                            Log.d("TAAAG", "angleInRadians: $angleInRadians")
+                            val x = centerPoint.first - (radius * cos(angleInRadians)).toFloat()
+                            Log.d("TAAAG", "x: $x")
+                            val y = centerPoint.second - (radius * sin(angleInRadians)).toFloat()
+                            Log.d("TAAAG", "y: $y")
+                            polygonEdgePoints.add(Pair(x, y))
+                        }
+                    }
+                }
+            }
+
+            polygonEdgePoints.forEachIndexed { index, edgePoint ->
+                it.lineTo(edgePoint.first, edgePoint.second)
+            }
+            it.close()
+            it
+        }
+
+        drawPath(path, fillColor)
+
+        drawPath(
+            path = path,
+            color = Color.Blue,
+            style = borderStyle
+        )
     }
 }
 
